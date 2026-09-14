70f9f88470e2ba37dc5a5f2199cc7fa1e6fc653d
RicardoLou <Anglyao778@gmail.com>
2026-09-14 10:45:11 +0800

    fix: keep storefront tabs visible on custom pages

--- apps/web/src/pages/StorefrontCustomPage.tsx
+++ apps/web/src/pages/StorefrontCustomPage.tsx
@@ -8,6 +8,7 @@ import { StorefrontExchangeRates } from "../components/StorefrontExchangeRates";
 import { StorefrontFooter } from "../components/StorefrontFooter";
 import { StorefrontLanguageSwitch } from "../components/StorefrontLanguageSwitch";
 import { StorefrontSupportWidget } from "../components/StorefrontSupportWidget";
+import { StorefrontTopNavigation } from "../components/StorefrontTopNavigation";
 import { StorefrontVisitorEntry } from "../components/StorefrontVisitorEntry";
 import { ThemeToggle } from "../components/ThemeToggle";
 import { prepareStorefrontCustomPageHtml } from "../lib/storefrontCustomPage";
@@ -182,6 +183,12 @@ export function StorefrontCustomPage() {
           </div>
         </Container>
       </header>
+      <StorefrontTopNavigation
+        store={store}
+        accountKey={accountKey}
+        locale={locale}
+        activePageSlug={page.slug}
+      />
       {page.exchange_rates_enabled ? (
         <StorefrontExchangeRates tenantSlug={store.slug} locale={locale} />
       ) : null}
